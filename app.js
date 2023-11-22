8f8d7161fdbba4a48d3c34a8ab9a2ff4479f8845
omar-ashraf2 <oashraf304@gmail.com>
2023-11-22 05:08:57 +0200

    commit

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import data from "./data.json" assert { type: "json" };
 const ul = document.getElementById("commentsList");
 const commentsList = document.getElementById("commentsContainer");
 let replyList = document.getElementById("replyContainer");
-
-
 let comments = data.comments;
 
 //Load Comments & Replies Data
@@ -82,8 +80,8 @@ const readComments = () => {
                 </div>
                 ${
                   replyUsername === "juliusomo"
-                    ? '<div class="editButtons"> <button class="replyButton danger"> <img src="./images/icon-delete.svg" alt="" /> Delete</button> <button class="replyButton"> <img src="./images/icon-edit.svg" alt="" /> Edit</button> </div>'
-                    : '<button class="replyButton" id="replyButton"> <img src="./images/icon-reply.svg" alt="" /> Reply</button>'
+                    ? '<div class="editButtons"> <button class="replyButton reply danger"> <img src="./images/icon-delete.svg" alt="" /> Delete</button> <button class="replyButton edit"> <img src="./images/icon-edit.svg" alt="" /> Edit</button> </div>'
+                    : '<button class="replyButton reply" id="replyButton"> <img src="./images/icon-reply.svg" alt="" /> Reply</button>'
                 }
               </div>
               <div class="bottomSection">
@@ -103,7 +101,11 @@ const readComments = () => {
 readComments();
 
 function addReply(text) {
+  let div = document.createElement("div");
+  div.setAttribute("class", "replyContainer");
   const newReply = document.createElement("li");
+  newReply.setAttribute("class", "commentBoxContainer replyBox");
+
   newReply.innerHTML += `<div class="vote">
     <a href="#"><img src="./images/icon-plus.svg" alt="" /></a>
     <span>5</span>
@@ -119,7 +121,7 @@ function addReply(text) {
           <h4>juliusomo <span class="you">you</span></h4>
           <span>Just Now</span>
       </div>
-      <div class="editButtons"> <button class="replyButton danger"> <img src="./images/icon-delete.svg" alt="" /> Delete</button> <button class="replyButton"> <img src="./images/icon-edit.svg" alt="" /> Edit</button> </div>
+      <div class="editButtons"> <button class="replyButton danger"> <img src="./images/icon-delete.svg" alt="" /> Delete</button> <button class="replyButton edit"> <img src="./images/icon-edit.svg" alt="" /> Edit</button> </div>
     </div>
     <div class="bottomSection">
         <p class="content">
@@ -128,52 +130,85 @@ function addReply(text) {
     </div>
   </div>`;
 
-  newReply.setAttribute("class", "commentBoxContainer replyBox");
-  replyList.appendChild(newReply);
-  return replyList;
+  div.appendChild(newReply);
+  return div;
 }
 
 ul.addEventListener("click", (e) => {
   const replyClicked = e.target.classList.contains("reply");
   const submitClicked = e.target.classList.contains("submit");
+  const editClicked = e.target.classList.contains("edit");
+  const updateClicked = e.target.classList.contains("submitButton");
   let closestCommentBox = e.target.closest(".commentBox");
+  let closestReply = e.target.closest(".replyBox");
+  const addNewComment = e.target.classList.contains("addNewComment");
 
   if (replyClicked) {
-    let li = document.createElement("li");
-    li.innerHTML = "";
-    if (replyList != "") {
-      // Creating Li
-      li.setAttribute("class", "replyBox addComment");
-      li.innerHTML += `<div class="imgContainer">
-      <img
-        class="replyAvatar"
-        src="./images/avatars/image-juliusomo.webp"
-        alt=""
-      />
-    </div>
-    <textarea
-      name="commentText"
-      class="commentText"
-      placeholder="Add a comment..."
-      rows="3"
-      required
-    ></textarea>
-    <button class="submit">SEND</button>`;
-
-      //Append in UL
-      replyList.appendChild(li);
-      closestCommentBox.appendChild(replyList);
+    // Creating Li & Container
+    let div = document.createElement("div");
+    div.setAttribute("class", "replyContainer");
+    if (closestCommentBox.children.length > 1) {
+      console.log(closestCommentBox.children);
     }
+    let li = document.createElement("li");
+    li.setAttribute("class", "replyBox addComment");
+    li.innerHTML += `<div class="imgContainer">
+        <img
+          class="replyAvatar"
+          src="./images/avatars/image-juliusomo.webp"
+          alt=""
+        />
+      </div>
+      <textarea
+        name="commentText"
+        class="textarea"
+        placeholder="Add a comment..."
+        rows="3"
+        required
+      ></textarea>
+      <button class="submit">SEND</button>`;
+
+    //Append in UL
+    div.appendChild(li);
+    closestCommentBox.appendChild(div);
   }
 
   if (submitClicked) {
-    let closestReplyBox = e.target.closest(".replyBox");
-    let inputText = closestReplyBox.children[1].value;
+    let closestReplyBox = e.target.closest(".replyContainer");
+    let closestReplyInput = e.target.closest(".replyBox");
+    let inputText = closestReplyInput.children[1].value;
     if (inputText != "") {
       closestCommentBox.appendChild(addReply(inputText));
       closestReplyBox.remove();
     }
   }
-});
 
-//Add a comment
+  function editComment(text) {
+    let editText = closestReply.querySelector(".content");
+    editText.innerHTML = `${text}`;
+    return editText;
+  }
+
+  if (editClicked) {
+    let editText = closestReply.querySelector(".content");
+    editText.innerHTML = `<textarea name="commentText" id="commentText" class="textarea" rows="3">${editText.innerText}</textarea>
+    <button class="submitButton">UPDATE</button>`;
+  }
+
+  if (updateClicked) {
+    let editText = closestReply.querySelector(".content");
+    let editTextInput = editText.children[0].value;
+    let bottomSection = e.target.closest(".bottomSection");
+    let updateBtn = e.target.closest(".submitButton");
+    let textArea = editText.children[0];
+
+    bottomSection.appendChild(editComment(editTextInput));
+    updateBtn.remove();
+    textArea.remove();
+  }
+
+  if (addNewComment) {
+    console.log("Hi");
+  }
+
+});
